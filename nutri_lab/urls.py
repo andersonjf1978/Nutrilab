9afe85a6d9260d26f5dde4cb2b314d9d28b72fbd
andersonjf1978 <andersonjf1978@gmail.com>
2023-08-07 21:35:51 -0300

    push

---
+++ nutri_lab/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('autenticacao.urls')),
+    path('', include('plataforma.urls')),
+]
+
+urlpatterns += + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ plataforma/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Pacientes(models.Model):
+    choices_sexo = (('F', 'Feminino'),
+                    ('M', 'Maculino'))
+    nome = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=1, choices=choices_sexo)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=19)
+    nutri = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+    
+
+class DadosPaciente(models.Model):
+    paciente = models.ForeignKey(Pacientes, on_delete=models.CASCADE)
+    data = models.DateTimeField()
+    peso = models.FloatField()
+    altura = models.IntegerField()
+    percentual_gordura = models.FloatField()
+    percentual_musculo = models.FloatField()
+    colesterol_hdl = models.FloatField()
+    colesterol_ldl = models.FloatField()
+    colesterol_total = models.FloatField()
+    trigliceridios = models.FloatField()
+    
+
+    def __str__(self):
+        return f"Paciente({self.paciente.nome}, {self.peso})"
+    
+class Refeicao(models.Model):
+    paciente = models.ForeignKey(Pacientes, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=50)
+    horario = models.TimeField()
+    carboidratos = models.FloatField()
+    proteinas = models.FloatField()
+    gorduras = models.FloatField()
+
+    def __str__(self):
+        return self.titulo
+
+
+class Opcao(models.Model):
+    refeicao = models.ForeignKey(Refeicao, on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to="opcao")
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.descricao
